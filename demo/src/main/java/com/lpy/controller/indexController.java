c31a05d18d7b1aea3f3cb453087ebfd543d7fbc5
xuchuncheng <1145304128@qq.com>
2019-05-06 15:56:27 +0800

    "ceshi"

---
+++ demo/src/main/java/com/lpy/controller/indexController.java
@@ -0,0 +1,14 @@
+package com.lpy.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 徐本六
+ * @Date: 2019/5/6 15:34
+ * @Version 1.0
+ * @maxim 蚍蜉撼大树 可敬不自量
+ */
+@RestController
+public class indexController {
+
+}
